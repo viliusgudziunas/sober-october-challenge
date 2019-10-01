c416dde6ecdd98f5764ef94848bbc9bfa8ed9c3a
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2019-10-01 23:44:35 +0100

    Deployment

--- project/__init__.py
+++ project/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config.from_object("project.config.DevelopmentConfig")
+app.config.from_object("project.config.ProductionConfig")
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- project/config.py
+++ project/config.py
@@ -31,4 +31,5 @@ class StagingConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or ""
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+        "postgres://xjcxnevjniqnfl:23610365df41885ab81b1a1ed895efe564f69f8b35622756563ef10274d877f4@ec2-107-22-222-161.compute-1.amazonaws.com:5432/d54r43ajqah3rn"
