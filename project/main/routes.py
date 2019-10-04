372538a9ab82cc39a82d1c7f55eab4c69297b066
viliusgudziunas <vilius.gudziunas@landregistry.gov.uk>
2019-10-04 16:23:29 +0100

    WIP: Working on history page

--- project/main/routes.py
+++ project/main/routes.py
@@ -1,3 +1,4 @@
+from datetime import date, timedelta
 from flask import render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 from project import db
@@ -9,6 +10,24 @@ from project.main.forms import ExerciseForm
 @bp.route("/")
 def index():
     return render_template("main/index.html")
+
+
+@bp.route("/add_exercise", methods=["GET", "POST"])
+@login_required
+def add_exercise():
+    form = ExerciseForm()
+    if form.validate_on_submit():
+        exercise = Exercise(
+            exercise=form.exercise.data,
+            calories=form.calories.data,
+            date=form.date.data,
+            author=current_user
+        )
+        db.session.add(exercise)
+        db.session.commit()
+        flash("Exercise submitted!")
+        return redirect(url_for("main.standings"))
+    return render_template("main/add_exercise.html", form=form)
 
 
 @bp.route("/standings")
@@ -29,19 +48,13 @@ def standings():
     return render_template("main/standings.html", standings=standings)
 
 
-@bp.route("/add_exercise", methods=["GET", "POST"])
+@bp.route("/history")
 @login_required
-def add_exercise():
-    form = ExerciseForm()
-    if form.validate_on_submit():
-        exercise = Exercise(
-            exercise=form.exercise.data,
-            calories=form.calories.data,
-            date=form.date.data,
-            author=current_user
-        )
-        db.session.add(exercise)
-        db.session.commit()
-        flash("Exercise submitted!")
-        return redirect(url_for("main.standings"))
-    return render_template("main/add_exercise.html", form=form)
+def history():
+    start_date = date(2019, 10, 1)
+    delta = date.today() - start_date
+    dates_in_october = []
+    for day in range(delta.days + 1):
+        dates_in_october.append(start_date + timedelta(days=day))
+    exercises = Exercise.query.all()
+    return render_template("main/history.html", dates=dates_in_october, exercises=exercises)
